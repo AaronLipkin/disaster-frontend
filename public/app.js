2faa8559291e19a3ac06f88e88c85d97d4f49c8f
Mark Murphy <mark@murphymark.me>
2017-09-08 11:19:59 -0500

    added this.notices array for AJAX responses

--- public/app.js
+++ public/app.js
@@ -1,13 +1,18 @@
 const app = angular.module('disasterboard', []);
+
 app.controller('mainController', ['$http', function($http) {
 // test message
 this.message = "controller works"
+this.notices = [];
 
 $http({
   method: 'GET',
 // need to set url
   url: 'http://localhost:3000/#'
-}).then(response => console.log(response))
+}).then(response => {
+  console.log(response);
+  this.notices = response.data;
+})
 .catch(err => console.log(err));
 
 // end of mainController
