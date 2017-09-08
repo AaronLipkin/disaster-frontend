f9ca0583c7ae20c5ca65c0aa8557cba0a496ff00
Mark Murphy <mark@murphymark.me>
2017-09-08 11:16:32 -0500

    set up AJAX GET request to rails server

--- public/app.js
+++ public/app.js
@@ -2,7 +2,15 @@ const app = angular.module('disasterboard', []);
 app.controller('mainController', ['$http', function($http) {
 // test message
 this.message = "controller works"
-// end of mainController 
+
+$http({
+  method: 'GET',
+// need to set url
+  url: 'http://localhost:3000/#'
+}).then(response => console.log(response))
+.catch(err => console.log(err));
+
+// end of mainController
 }]);
 
 // test message to confirm app.js is linked
