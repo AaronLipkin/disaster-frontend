0d1c1b9408653e507e607401cbb465a30a0ce3b3
Mark Murphy <mark@murphymark.me>
2017-09-08 11:12:59 -0500

    set module and main controller and confirmed they work

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
+const app = angular.module('disasterboard', []);
+app.controller('mainController', ['$http', function($http) {
+// test message
+this.message = "controller works"
+// end of mainController 
+}]);
+
+// test message to confirm app.js is linked
 console.log('app.js working');
