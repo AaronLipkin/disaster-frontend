60e8f83512fe3ae91a74348157765a78b58571ee
Mark Murphy <mark@murphymark.me>
2017-09-08 11:28:20 -0500

    added index outline and ability to capture form data

--- public/app.js
+++ public/app.js
@@ -2,18 +2,24 @@ const app = angular.module('disasterboard', []);
 
 app.controller('mainController', ['$http', function($http) {
 // test message
-this.message = "controller works"
-this.notices = [];
+  this.message = "controller works"
+  this.notices = [];
+  this.formdata = {};
 
-$http({
-  method: 'GET',
-// need to set url
-  url: 'http://localhost:3000/#'
-}).then(response => {
-  console.log(response);
-  this.notices = response.data;
-})
-.catch(err => console.log(err));
+  $http({
+    method: 'GET',
+  // need to set url
+    url: 'http://localhost:3000/#'
+  }).then(response => {
+    console.log(response);
+    this.notices = response.data;
+  })
+  .catch(err => console.log(err));
+
+  this.processForm = function() {
+  	console.log('processForm function . . .');
+  	console.log('Formdata: ', this.formdata);
+  }
 
 // end of mainController
 }]);
